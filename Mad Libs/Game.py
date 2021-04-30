79b1deac87c1ae00ccd0d6447a384cfab6e5132a
Aditya Patil <83420792+AdityaPatil001@users.noreply.github.com>
2021-05-01 00:17:01 +0530

    Update Game.py

--- Mad Libs/Game.py
+++ Mad Libs/Game.py
@@ -4,4 +4,4 @@ celebrity = input("Enter a celebrity: ")
 
 print("Roses are " + color)
 print(plural_noun + " are blue")
-print("I love " + celebrity)
+print("My Dear " + celebrity + ", I love you" )
